092f8bd7bd93c6d905700bcf82e4045fd47e6221
chi <chisanfung@gmail.com>
2020-01-26 01:02:24 -0500

    Now ParadoxTable can read several types of fields

--- index.js
+++ index.js
@@ -2,10 +2,74 @@ const fs = require("fs")
 var argv = require('minimist')(process.argv.slice(1));
 var { byte, integer, longint, char, pchar, word, pointer, TFldInfoRec } = require("./src/types")
 
+function unsetBit(buffer) {
+    var b = buffer
+    var firstByte = b[0] - 2 ** (4 * 2 - 1)
 
+    if (b.length === 1) {
+        if (buffer.readUInt8() !== 0) {
+            b.writeUInt8(firstByte)
+        } else {
+            return 0
+        }
 
-class ParadoxHeader {
+    } else if (b.length === 2) {
+        if (buffer.readUInt16BE() !== 0) {
+            b.writeUInt8(firstByte)
+        } else {
+            return 0
+        }
+    } else if (b.length === 4) {
+        if (buffer.readUInt16BE() !== 0) {
+            b.writeUInt8(firstByte)
+        } else {
+            return 0
+        }
+    } else {
+        if (buffer.readDoubleBE() !== 0) {
+            b.writeUInt8(firstByte)
+        } else {
+            return 0
+        }
+    }
+
+    return b
+}
+
+function convertTime() {
+    var b = unsetBit(buffer).readUInt16BE()
+    // TODO
+}
+
+function convertTimestamp(buffer, dateOffset = 0) {
+    var b = unsetBit(buffer).readDoubleBE()
+    var d = new Date("0001-01-01")
+    d.setHours(0)
+    d.setMinutes(0)
+    d.setSeconds(0)
+    //console.log(d.toUTCString(), b)
+    
+    var date = (new Date(d.getTime()+b+dateOffset))//.getTime()
+    //console.log(date.toUTCString())
+    return date
+}
+
+function convertDate(buffer) {
+    var date = new Date("0001-01-01");
+    var days = unsetBit(buffer)
+    
+    if (days) {
+        date.setDate(date.getDate() + days.readUInt32BE());
+        //console.log(date.getDate(), date, days)
+        return date;
+    }
+
+    return 0
+}
+
+class ParadoxTable {
     constructor(buffer) {
+        this.buffer = buffer
         this.recordSize = new integer(buffer, 0, false)
 
         this.headerSize = new integer(buffer, this.recordSize.upperlimmit, false)
@@ -18,7 +82,7 @@ class ParadoxHeader {
 
         this.nextBlock = new word(buffer, this.numRecords.upperlimmit)
 
-        this.fileBlocks = new word(buffer, this.nextBlock.upperlimmit)
+        this.fileBlocks = new word(buffer, this.nextBlock.upperlimmit, false)
 
         this.firstBlock = new word(buffer, this.fileBlocks.upperlimmit)
 
@@ -110,12 +174,12 @@ class ParadoxHeader {
 
         var TFldInfoRecArray = []
 
-        for (var i = parseInt(78, 16); i < (parseInt(78, 16) + this.numFields.getValue()* 2) ; i += 2) {
+        for (var i = parseInt(78, 16); i < (parseInt(78, 16) + this.numFields.getValue() * 2); i += 2) {
             //console.log(i.toString(16))
             TFldInfoRecArray.push(new TFldInfoRec(buffer, i))
         }
 
-        
+
 
         var pcharArray = []
 
@@ -141,13 +205,13 @@ class ParadoxHeader {
 
         var blockLimit = initialDBNameEnd
 
-        while(buffer[blockLimit] === 0) {
+        while (buffer[blockLimit] === 0) {
             blockLimit++
         }
 
         var initialBlockLimit = blockLimit
 
-        while(buffer[blockLimit] >= 30  || buffer[blockLimit+1] >= 30) {
+        while (buffer[blockLimit] >= 30 || buffer[blockLimit + 1] >= 30) {
             blockLimit++
         }
 
@@ -155,32 +219,161 @@ class ParadoxHeader {
 
         this.fieldArray = fieldsString.split('\u0000')
 
-        for(var k in TFldInfoRecArray){
+        for (var k in TFldInfoRecArray) {
             TFldInfoRecArray[k].addName(this.fieldArray[k])
         }
 
         this.TFldInfoRecArray = TFldInfoRecArray
 
+        this.firstaddDataSize = this.buffer.slice(this.headerSize.getValue() + 4, this.headerSize.getValue() + 6)
+
         //there must be a better way to find the portion of the .db file which contains all the field names
 
         //I decided to skip what comes after the field names, see /documents/PxFORMAT.txt . I'm assuming that the database to be read is unencrypted
     }
+
+    returnRecords(nb, dateOffset = 0) {
+        var recordsStart = this.headerSize.getValue() + 6
+        var records = []
+        var blockSize = this.maxTableSize.getValue() * 1024
+
+        var getAddDataSize = (buff, offset) => buff.slice(offset + 4, offset + 6)
+
+        var numberOfBlocks = nb || this.fileBlocks.getValue()
+
+        //Go through each block
+        for (var i = 0; i < numberOfBlocks; i++) {
+            var addDataSize = getAddDataSize(this.buffer, this.headerSize.getValue() + blockSize * i)
+            var numRecordsInBlock = addDataSize.readUInt16LE() / this.recordSize.getValue() + 1
+            var recordsStart = this.headerSize.getValue() + blockSize * i + 6
+
+            //Go through each record
+            for (var j = 0; j < numRecordsInBlock; j++) {
+                var record = []
+
+                //Go through each field
+                for (var k = 0; k < this.TFldInfoRecArray.length; k++) {
+
+                    record.push(new Field(this.TFldInfoRecArray[k].name,
+                        this.TFldInfoRecArray[k].getType(),
+                        this.buffer.slice(recordsStart,
+                            recordsStart + this.TFldInfoRecArray[k].getSize()),"ascii",dateOffset
+                    )
+                    )
+                    recordsStart += this.TFldInfoRecArray[k].getSize()
+                }
+                records.push(record)
+            }
+        }
+        return records
+
+    }
+}
+
+class Field {
+    constructor(name, type, value, encoding = "ascii", dateOffset = 0) {
+        this.name = name
+        this.type = type
+        this.valueBuffer = value
+        this.hasBeenProperlyDecoded = true
+        //                         |      |           fType  fSize(decimal)                                     |
+        switch (this.type) {
+            case 1:
+                //        |      |            $01     v   "A"  Alpha                                   |
+                this.value = value.toString(encoding)
+                break
+            case 2:
+                //        |      |            $02     4   "D"  Date                                    |
+                this.value = convertDate(this.valueBuffer)
+                break
+            case 3:
+                //        |      |            $03     2   "S"  Short integer                           |
+                var test = unsetBit(this.valueBuffer)
+                this.value = test ? test.readUInt16LE() : test
+                break
+            case 4:
+                //        |      |            $04     4   "I"  Long integer                            |
+                var test = unsetBit(this.valueBuffer)
+                this.value = test ? test.readUInt32LE() : test
+                break
+            case 5:
+            //        |      |            $05     8   "$"  currency                                |
+            case 6:
+                //        |      |            $06     8   "N"  Number                                  |
+                this.value = -value.readDoubleBE()
+                break
+            case 9:
+                //        |      |            $09     1   "L"  Logical                                 |
+                var test = unsetBit(this.valueBuffer)
+
+                if (!test) {
+                    this.hasBeenProperlyDecoded = false
+                    //I'm not sure what it means when the boolean is completely filled with zeroes i.e 0x00 or 00000000
+                }
+                this.value = test ? test.readUInt8() : test
+
+                break
+            case 20:
+                //        |      |            $14     4   "T"  Time                                    |
+                throw '"Time" data type is currently unsupported!'
+                //break
+            case 21:
+                //        |      |            $15     8   "@"  Timestamp                               |
+                this.hasBeenProperlyDecoded = false
+                //I have noticed that the timestamps are off by a few minutes, so there is dateOffset to correct for that
+
+                this.value = convertTimestamp(this.valueBuffer, dateOffset)
+                break
+            case 22:
+                //        |      |            $16     4   "+"  Autoincrement                           |
+                var test = unsetBit(this.valueBuffer)
+                this.hasBeenProperlyDecoded = false
+                this.value = test ? test.readUInt32LE() : test
+                break
+            case 12:
+            //        |      |            $0C     v   "M"  Memo BLOb                               |
+            case 13:
+            //        |      |            $0D     v   "B"  Binary Large Object                     |
+            case 14:
+            //        |      |            $0E     v   "F"  Formatted Memo BLOb                     |
+            case 15:
+            //        |      |            $0F     v   "O"  OLE                                     |
+            case 16:
+            //        |      |            $10     v   "G"  Graphic BLOb                            |
+            case 23:
+            //        |      |            $17    17*  "#"  BCD                                     |
+            case 24:
+                //        |      |            $18     v   "Y"  Bytes                                   |
+
+                //I will leave the decoding of these data types for the user of this library because I guess it depends on the type of data stored, unless you'd like to contribute to this proyect
+                this.hasBeenProperlyDecoded = false
+                this.value = this.valueBuffer
+                break
+            default:
+                this.hasBeenProperlyDecoded = false
+                this.value = this.valueBuffer
+                break
+        }
+    }
+}
+
+function dumpToCSV(table){
+    // TODO
 }
 
 if (!module.parent) {
-    // this is the main module
     if (argv.f) {
         var file = fs.readFileSync(argv.f)
-        var h = new ParadoxHeader(file)
-        h.TFldInfoRecArray.forEach(x=>{
-            console.log(x.name, x.getSize())
-        })
+        var t = new ParadoxTable(file)
+        dumpToCSV(table)
+    } else {
+        console.log("-f argument required")
     }
 
 }
 
 
-
+module.exports = ParadoxTable
 
 
 
